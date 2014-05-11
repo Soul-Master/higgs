7f58be9fbf038506374665d0469caa54484737d1
Soul_Master <Soul_Master@live.com>
2014-05-12 03:21:50 +0700

    Add TrimString Model Binder.

---
+++ Higgs.Web/TrimStringBinder.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.ModelBinding;
+using System.Web.Mvc;
+
+namespace Higgs.Web
+{
+    public class TrimStringBinder : System.Web.ModelBinding.IModelBinder
+    {
+        public bool BindModel(ModelBindingExecutionContext modelBindingExecutionContext, System.Web.ModelBinding.ModelBindingContext bindingContext)
+        {
+            var valueResult = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+
+            if (valueResult == null || string.IsNullOrEmpty(valueResult.AttemptedValue))
+            {
+                bindingContext.Model = null;
+            }
+            else
+            {
+                bindingContext.Model = valueResult.AttemptedValue.Trim();
+            }
+
+            return true;
+        }
+    }
+}
