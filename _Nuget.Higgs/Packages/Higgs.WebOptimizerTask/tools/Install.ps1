125a50bb1e24caf31154336c13d3d7ae1f3bdb6b
Soul_Master <Soul_Master@live.com>
2022-10-04 19:36:11 +0700

    Temp

--- _Nuget.Higgs/Packages/Higgs.WebOptimizerTask/tools/Install.ps1
+++ _Nuget.Higgs/Packages/Higgs.WebOptimizerTask/tools/Install.ps1
@@ -1,5 +1,3 @@
-﻿npm install grunt -g
-npm install grunt-cli -g
-npm install clean-css -g
-npm install clean-css-cli -g
+﻿npm install clean-css@4.2.3 -g
+npm install clean-css-cli@4.3.0 -g
 npm install uglify-js2 -g
